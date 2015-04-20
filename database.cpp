ecd401228e3e3f5e43ded000125e508bf49bd083
Jarvis Prestidge <jarvisprestidge@gmail.com>
2015-04-20 09:13:25 +0100

    Preparing for branch.

--- database.cpp
+++ database.cpp
@@ -18,42 +18,42 @@ Database::~Database() {
 
 // Add a new composer using operations in the Composer class.
 // For convenience, we return a reference (pointer) to the new record.
-Composer& Database::AddComposer(string in_first_name, 
+Composer* Database::AddComposer(string in_first_name, 
 																string in_last_name,
 																string in_genre, 
 																int in_yob, 
 																string in_fact) {
 	// Creating a new composer object
-	Composer composer;
+	Composer* composer = Composer;
 
-  composer.set_first_name(in_first_name);
-  composer.set_last_name(in_last_name);
-  composer.set_composer_yob(in_yob);
-  composer.set_composer_genre(in_genre);
-  composer.set_fact(in_fact);
+  composer->set_first_name(in_first_name);
+  composer->set_last_name(in_last_name);
+  composer->set_composer_yob(in_yob);
+  composer->set_composer_genre(in_genre);
+  composer->set_fact(in_fact);
 
   // Adding the newly created composer object to the composers_ array
-  composers_[next_slot_] = composer;
+  composers_[next_slot_] = *composer;
 
   // Increment the next_slot_ counter
   next_slot_ ++;
 
   // Might need to set this to a reference
-  return composers_[(next_slot_)-1];
+  return composer;
 }
 
 // Search for a composer based on last name. Return a reference to the
 // found record.
-Composer& Database::GetComposer(string in_last_name) {
+Composer* Database::GetComposer(string in_last_name) {
 	// Iterating through the composer_ array 
 	for (int i = 0; i < next_slot_; ++i) {
 
 		if (composers_[i].last_name() == in_last_name) {
 			// Return the reference to the matching composer object
-			return composers_[i];
+			return &composers_[i];
 		}
 	}
-	return;
+	return nullptr;
 }
 
 // Display all composers in the database.
