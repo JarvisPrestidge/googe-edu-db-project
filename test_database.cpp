345b3d2eb4a8af59d72432e05ee1cf2212857394
Jarvis Prestidge <jarvisprestidge@gmail.com>
2015-04-20 10:33:52 +0100

    Reverting commit, to fix AddComposer().

--- test_database.cpp
+++ test_database.cpp
@@ -9,20 +9,19 @@ int main() {
   Database myDB;
 
   // Remember that AddComposer returns a reference to the new record.
-  Composer* comp1 = myDB.AddComposer("Ludwig van", "Beethoven", "Romantic", 1770,
+  Composer& comp1 = myDB.AddComposer("Ludwig van", "Beethoven", "Romantic", 1770,
     "Beethoven was completely deaf during the latter part of his life - he never "
     "heard a performance of his 9th symphony.");
-  comp1->Display();
-  comp1->Promote(7);
+  comp1.Promote(7);
 
-  Composer* comp2 = myDB.AddComposer("Johann Sebastian", "Bach", "Baroque", 1685,
+  Composer& comp2 = myDB.AddComposer("Johann Sebastian", "Bach", "Baroque", 1685,
     "Bach had 20 children, several of whom became famous musicians as well.");
-  comp2->Promote(5);
+  comp2.Promote(5);
 
-  Composer* comp3 = myDB.AddComposer("Wolfgang Amadeus", "Mozart", "Classical", 1756,
+  Composer& comp3 = myDB.AddComposer("Wolfgang Amadeus", "Mozart", "Classical", 1756,
     "Mozart feared for his life during his last year - there is some evidence "
     "that he was poisoned.");
-  comp3->Promote(2);
+  comp3.Promote(2);
 
   cout << endl << "all Composers: " << endl << endl;
   myDB.DisplayAll();
